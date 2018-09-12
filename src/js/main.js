a8a5a8a1c90974029dd04606354d99b22561fbf2
Ian Fajardo <ianfajardozx@gmail.com>
2018-09-12 13:15:36 -0400

    Add reading time plugin

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,8 @@ var latestSliderWidth = 100;
 var maxSliderHeight = 0;
 var latestTechPosition = 0;
 
+!function(e){e.fn.readingTime=function(n){var t={readingTimeTarget:".eta",wordCountTarget:null,wordsPerMinute:270,round:!0,lang:"en",lessThanAMinuteString:"",prependTimeString:"",prependWordString:"",remotePath:null,remoteTarget:null,success:function(){},error:function(){}},i=this,r=e(this);i.settings=e.extend({},t,n);var a=i.settings;if(!this.length)return a.error.call(this),this;if("it"==a.lang)var s=a.lessThanAMinuteString||"Meno di un minuto",l="min";else if("fr"==a.lang)var s=a.lessThanAMinuteString||"Moins d'une minute",l="min";else if("de"==a.lang)var s=a.lessThanAMinuteString||"Weniger als eine Minute",l="min";else if("es"==a.lang)var s=a.lessThanAMinuteString||"Menos de un minuto",l="min";else if("nl"==a.lang)var s=a.lessThanAMinuteString||"Minder dan een minuut",l="min";else if("sk"==a.lang)var s=a.lessThanAMinuteString||"Menej neÅ¾ minÃºtu",l="min";else if("cz"==a.lang)var s=a.lessThanAMinuteString||"MÃ©nÄ› neÅ¾ minutu",l="min";else if("hu"==a.lang)var s=a.lessThanAMinuteString||"Kevesebb mint egy perc",l="perc";else var s=a.lessThanAMinuteString||"Less than a minute",l="min";var u=function(n){if(""!==n){var t=n.trim().split(/\s+/g).length,i=a.wordsPerMinute/60,r=t/i;if(a.round===!0)var u=Math.round(r/60);else var u=Math.floor(r/60);var g=Math.round(r-60*u);if(a.round===!0)e(a.readingTimeTarget).text(u>0?a.prependTimeString+u+" "+l:a.prependTimeString+s);else{var o=u+":"+g;e(a.readingTimeTarget).text(a.prependTimeString+o)}""!==a.wordCountTarget&&void 0!==a.wordCountTarget&&e(a.wordCountTarget).text(a.prependWordString+t),a.success.call(this)}else a.error.call(this,"The element is empty.")};r.each(function(){null!=a.remotePath&&null!=a.remoteTarget?e.get(a.remotePath,function(n){u(e("<div>").html(n).find(a.remoteTarget).text())}):u(r.text())})}}(jQuery);
+
 function latestSliderInit(){
 
   $(".homePageGrid .content-block--pageItem__inside").each(function(){
@@ -107,9 +109,10 @@ function productRandomize() {
 }
 
 
-function articleSocialInit(){
+function readingTime(){
   if( $(".article--headline").length ) {
-
+    $(".author-byline .metadata").append('<li><span class="eta"></span> Read (<span class="word-count"></span> Words)</li>');
+    $(".article-body").readingTime({wordCountTarget: ".word-count"});
   }
 }
 
@@ -310,13 +313,12 @@ var app = new Vue({
 //functions for just the homepage
 if($("body").hasClass("home-page")){
   latestSliderInit();
-
-  //techInit();
   quoteRandomize();
   productRandomize();
 }
 
 articleSocial();
+readingTime();
 
 $(".supernav-mobile-icon .icon-menu").click(function(){
   $(".supernav-mobile-menu").toggle();
