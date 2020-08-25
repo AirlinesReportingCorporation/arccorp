c8d573f34f4cf7e3d69b11a906a1241e496c846c
Ian Fajardo <ianfajardozx@gmail.com>
2020-08-25 11:10:57 -0400

    update latest carousel static

--- src/js/main.js
+++ src/js/main.js
@@ -211,7 +211,7 @@ function latestSliderInit() {
   $(".homePageGrid .page-grid__row")
     .eq(0)
     .prepend(
-      "<div class='featuredLatest featuredLatest-custom content-block--pageItem'><div class=''><a target='_blank' href='https://www.surveygizmo.com/s3/5736700/ARC-TravelConnect-Virtual-Series-What-s-Next-for-Business-Travel?utm_source=thelatest_static'><img src='https://www2.arccorp.com/globalassets/homepage/redesign/The-Latest-TC-Episode3.png'></a></div></div> "
+      "<div class='featuredLatest featuredLatest-custom content-block--pageItem'><div class=''><a target='_blank' href='https://arctravelconnect.com/2020/episodes/Whats-Next-for-Business-Travel/'><img src='https://www2.arccorp.com/globalassets/homepage/redesign/the-latest-tc-episode-3.png'></a></div></div> "
     );
 
   $(".content-block--pageItem__inside").prepend("");
