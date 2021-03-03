518e9f342496a03e2239392a14bfb59d32888325
Ian Fajardo <ianfajardozx@gmail.com>
2021-03-03 16:07:38 -0500

    update test

--- my_functions/twitterAPI.js
+++ my_functions/twitterAPI.js
@@ -1,27 +1,8 @@
 const Twit = require("twit");
 
 exports.handler = async (event, context) => {
-  var T = new Twit({
-    consumer_key: "onFsZbUz23wNU21wDEqGcHbf0",
-    consumer_secret: "D6f8gaMfemGPfM6u73lLOZUooCYZwLzM8XLdnSXjlOse5p9dTs",
-    access_token: "224845357-zhS1TiAlQo6JbQmbGOC9QeZzpQsB1asc48XVmdAt",
-    access_token_secret: "Y9caJI6a8GOzmYmcjuKFFE2B4H9J7qFTZ3AdSwO1U2yZe",
-    timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
-  });
-
-  T.get(
-    "statuses/user_timeline",
-    { screen_name: "ARCtalk", count: 20, tweet_mode: "extended" },
-    function(err, data, response) {
-      return {
-        statusCode: 400,
-        headers: {
-          "Access-Control-Allow-Headers":
-            "Origin, X-Requested-With, Content-Type, Accept",
-          "Access-Control-Allow-Origin": "*"
-        },
-        body: JSON.stringify(data)
-      };
-    }
-  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" })
+  };
 };
