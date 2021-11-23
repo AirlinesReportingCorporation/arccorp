37c5ba4402cba55df105c1d90b29af7727bb3b18
Ian Fajardo <ianfajardozx@gmail.com>
2021-11-23 14:55:02 -0500

    update packages to match dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,23 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   //devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    watchContentBase: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true
+      }
+    },
+    static: {
+      directory: path.join(__dirname, "src")
+    },
+
+    compress: true,
     hot: true,
-    overlay: true
-  },
+    open: true
+  }
 });
