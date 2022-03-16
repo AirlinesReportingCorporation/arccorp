51715bcd9626b559d15cbded348561bb656bbabf
Ian Fajardo <ianfajardozx@gmail.com>
2022-03-16 16:02:34 -0400

    udpate with footer component mount

---
+++ src/components/Footer.jsx
@@ -0,0 +1,313 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="arc-footer">
+        <div className="text-center arc-footer-container-base">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="arc-footer-sub">
+                <div className="arc-footer-tagline">
+                  The Intelligence Behind Air Travel
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="arc-footer-container">
+          <div className="row">
+            <div className="col-lg col-md-4 col-sm-6">
+              <div className="arc-footer-section">
+                <div className="arc-footer-title">Participation</div>
+                <a
+                  href="https://www2.arccorp.com/support-training/travel-agencies/participating-airline-information?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Participating Airlines
+                </a>
+                <a
+                  href="https://www2.arccorp.com/support-training/arc-check-results?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Check
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/travel-agencies/agency-participation/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Agency Accreditation
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/travel-agencies/become-an-arc-accredited-agent/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Become an ARC-Accredited Agency
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/travel-agencies/become-an-arc-verified-travel-consultant/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Become an ARC VTC
+                </a>
+              </div>
+            </div>
+            <div className="col-lg col-md-4 col-sm-6">
+              <div className="arc-footer-section">
+                <div className="arc-footer-title">
+                  Distribution & Financial Solutions
+                </div>
+                <a
+                  href="https://www2.arccorp.com/products-participation/distribution/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Direct Connect (NDC)
+                </a>
+                <a
+                  href="https://www2.arccorp.com/about-us/arc-omnichannel-retailing/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Omnichannel Retailing
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/products/arc-pay/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Payment Processing
+                </a>
+                <a
+                  href="https://www2.arccorp.com/about-us/settlement-services/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Settlement Services
+                </a>
+              </div>
+            </div>
+            <div className="col-lg col-md-4 col-sm-6">
+              <div className="arc-footer-section">
+                <div className="arc-footer-title">Data Solutions</div>
+                <a
+                  href="https://www2.arccorp.com/about-us/arc-data/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Our Data
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/products/arc-data-connect/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Data Connect
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/products/arc-travel-demand/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Travel Demand
+                </a>
+                <a
+                  href="https://www2.arccorp.com/products-participation/products/direct-data-solutions/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Direct Data Solutions
+                </a>
+              </div>
+            </div>
+            <div className="col-lg col-md-4 col-sm-6">
+              <div className="arc-footer-section">
+                <div className="arc-footer-title">Communications</div>
+                <a
+                  href="https://www2.arccorp.com/about-us/newsroom/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Newsroom
+                </a>
+                <a
+                  href="https://www2.arccorp.com/arc-talk-podcast?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC Talk Podcast
+                </a>
+                <a
+                  href="https://arctravelconnect.com?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  ARC TravelConnect
+                </a>
+                <a
+                  href="https://www2.arccorp.com/articles-trends/blog/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Blog
+                </a>
+                <a
+                  href="https://www2.arccorp.com/articles-trends/agency-communications/travel-agent-communications/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Travel Agent Communications
+                </a>
+              </div>
+            </div>
+            <div className="col-lg col-md-4 col-sm-6">
+              <div className="arc-footer-section">
+                <div className="arc-footer-title">About Us</div>
+                <a
+                  href="https://www2.arccorp.com/about-us/our-story/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Our Story
+                </a>
+                <a
+                  href="https://www2.arccorp.com/about-us/careers/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Careers
+                </a>
+                <a
+                  href="https://www2.arccorp.com/about-us/leadership-governance/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Leadership & Governance
+                </a>
+                <a
+                  href="https://www2.arccorp.com/about-us/newsroom/media-kit/?utm_source=footer-link"
+                  className="arc-footer-link"
+                >
+                  Media Kit
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="arc-footer-bottom">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="arc-footer-sub-border"></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-7">
+              &copy; {new Date().getFullYear()} Airlines Reporting Corporation
+              (ARC). All rights reserved.
+            </div>
+            <div className="col-lg-2 text-sm-center">
+              <a
+                href="https://www.schellman.com/certificate-directory?certificateNumber=1427462-10"
+                target="_blank"
+                className="arc-footer-iso"
+              >
+                <img
+                  src="https://www2.arccorp.com/globalassets/homepage/redesign/march-update/iso_logo.png"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 text-sm-center">
+              <ul className="arc-footer-links-social d-inline-block">
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/arc"
+                    title="LinkedIn"
+                    target="_blank"
+                    className="arc-footer-icon"
+                  >
+                    <img
+                      src="https://www2.arccorp.com/globalassets/homepage/redesign/march-update/linkedin_logo.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/ARCtalk"
+                    title="twitter"
+                    target="_blank"
+                    className="arc-footer-icon"
+                  >
+                    <img
+                      src="https://www2.arccorp.com/globalassets/homepage/redesign/march-update/twitter_logo.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.facebook.com/ARCtalk"
+                    title="facebook"
+                    target="_blank"
+                    className="arc-footer-icon"
+                  >
+                    <img
+                      src="https://www2.arccorp.com/globalassets/homepage/redesign/march-update/facebook_logo.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UCWHZsoCDNhjorop892EVETw"
+                    target="_blank"
+                    className="arc-footer-icon"
+                  >
+                    <img
+                      src="https://www2.arccorp.com/globalassets/homepage/redesign/march-update/youtube_logo.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="bg-color-tarmac">
+          <div className="arc-footer-links-policy">
+            <div className="row">
+              <div className="col-lg-12">
+                <span className="footer-rtf-links">
+                  <a
+                    href="https://www2.arccorp.com/website-terms-of-use/"
+                    title="Terms"
+                  >
+                    Terms
+                  </a>
+
+                  <a
+                    href="https://www2.arccorp.com/site-privacy-policy/"
+                    title="Privacy"
+                  >
+                    Privacy
+                  </a>
+
+                  <a
+                    href="https://www2.arccorp.com/pci-data-security-standards/"
+                    title="PCI Standards"
+                  >
+                    PCI Standards
+                  </a>
+
+                  <a
+                    href="https://arccorp.statusdashboard.com/"
+                    title="ARC continually monitors the status of ARCCorp.com and related services. If a service is interrupted or is temporarily down for maintenance, an alert is posted here. If you wish to receive an email alert for specific services (or for all services), subscribe here."
+                    target="_blank"
+                  >
+                    ARC System Status
+                  </a>
+                  <a href="https://www2.arccorp.com/support-training/data-subject-access-request-form/">
+                    Do Not Sell My Personal Information
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Apphome from './apphome.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Apphome from "./apphome.jsx";
+import Footer from "./components/Footer";
 
-var mountNode = document.getElementById('home-app');
+var mountNode = document.getElementById("home-app");
+var footerNode = document.getElementById("arc-footer");
 
 ReactDOM.render(<Apphome />, mountNode);
+ReactDOM.render(<Footer />, footerNode);
