1dba355b4869d1bc5f151abd40b22b592923f97b
Ian Fajardo <ianfajardozx@gmail.com>
2022-12-15 16:06:22 -0500

    update jumbo to november press release

--- src/apphome.jsx
+++ src/apphome.jsx
@@ -86,7 +86,7 @@ class Apphome extends Component {
       <SwiperSlide>
         <div
           className="youtube-background"
-          data-vbg="https://www2.arccorp.com/globalassets/video/november-homepage-2x.mp4"
+          data-vbg="https://www2.arccorp.com/globalassets/video/december-homepage-2x.mp4"
         >
           <div
             className="arc-jumbo transparent"
@@ -103,17 +103,15 @@ class Apphome extends Component {
                   {}
 
                   <h2 className="arc-jumbo-inner-title white mb-1">
-                    October Ancillary Sales by U.S. Air Travel Agencies Increase
-                    113% YoY
+                    Passenger Trips Increase 14% YoY in November
                   </h2>
                   <div
                     className="arc-jumbo-inner-body"
                     style={{ marginBottom: "60px" }}
                   >
-                    Air Ticket Sales Totaled $7.3 Billion While International
-                    Passenger Trips Increased 37%{" "}
+                    International Passenger Trips Increased 32% Year over Year{" "}
                     <a className="arc-homepage-cta-link arc-jumbo-cta-link d-inline-flex p-0 pl-2">
-                      <a href="https://www2.arccorp.com/about-us/newsroom/2022-news-releases/october-ancillary-sales-by-us-air-travel-agencies/?utm_source=Jumbo_Carousel">
+                      <a href="https://www2.arccorp.com/about-us/newsroom/2022-news-releases/passenger-trips-in-november-2022/?utm_source=Jumbo_Carousel">
                         Read More
                       </a>
                     </a>
