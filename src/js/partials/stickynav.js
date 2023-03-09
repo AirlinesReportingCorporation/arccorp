6265acd8d3ef2cb60580de19e96d45c2bd8220f5
Ian Fajardo <ianfajardozx@gmail.com>
2023-03-09 13:34:42 -0500

    init commit

---
+++ src/js/partials/stickynav.js
@@ -0,0 +1,44 @@
+function stickynav() {
+  var stickyExists = $(".sticky-wrapper").length > 0;
+  if (stickyExists) {
+    $(document).ready(function() {
+      var sticky = $(".sticky-wrapper");
+
+      $(".navcontainer").after(sticky);
+      $(".sticky-wrapper").show();
+
+      var product;
+
+      if (
+        $("h2.block-header")
+          .html()
+          .includes("ADXMI")
+      ) {
+        product = "ADXMI";
+      } else if (
+        $("h2.block-header")
+          .html()
+          .includes("ODSS")
+      ) {
+        product = "ODSS";
+      } else if (
+        $("h2.block-header")
+          .html()
+          .includes("ARC's IRS 1099 Processing Service")
+      ) {
+        product = "IRS 1099 Processing Service";
+      } else {
+        product = $("h2.block-header").html();
+      }
+      console.log("Product: " + product);
+
+      $(".sticky-nav__linksList").before(
+        "<div class='sticky-nav__Branding'><img src='https://www2.arccorp.com/globalassets/arc-logos/corporate-logos/arc-logo-s-white.png' alt='ARC Logo' class='sticky-nav__arcLogo'><div class='sticky-nav__Productname'>" +
+          product +
+          "</div></div>"
+      );
+    });
+  }
+}
+
+stickynav();
