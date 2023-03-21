b52db3825ba0b8a53fc6080803324ac00b378ecc
Bree Armstrong <brianna.m.armstrong@gmail.com>
2023-03-21 16:39:17 -0400

    work on getting the stickynav from arccorp to work on a page

--- src/js/partials/stickynav.js
+++ src/js/partials/stickynav.js
@@ -1,14 +1,30 @@
 import React from "react";
+import {Stickynav} from 'arccorp-vars';
 
 export default function ARCStickyNav () {
+  let pageTitle = document.querySelector(".mainTitle").innerText;
+  console.log(pageTitle);
+  let pageLinks = document.querySelectorAll('.sticky-nav__link');
+  console.log(pageLinks);
+  let links=[]
+  pageLinks.forEach((pageLink)=> {
+    const newLink = {
+      link : pageLink.href.split("#")[1],
+      title : pageLink.innerText
+    }
+    links.push(newLink)
+  })
+  console.log(links);
   document.querySelector(".sticky-wrapper").style.display='none';
   return (
+    <Stickynav
+    link={links}
+    title={pageTitle}/>
 
     // grab all instances of the sticky nav items
     // grab the cta
 
     // put them into the arccorp-vars stickynav
-    <p>Hello</p>
   )
 }
 
@@ -32,60 +48,60 @@ export default function ARCStickyNav () {
 
 
 
-function stickynav() {
-  $(document).ready(function () {
-    document.querySelector(".sticky-wrapper").style.display='none';
-    // var stickyExists = $(".sticky-wrapper").length > 0;
-    // console.log("hello from sticky" + stickyExists);
-    // if (stickyExists) {
-    //   $(".supernav").css("border-bottom", "none");
-    //   var sticky = $(".sticky-wrapper").html();
-    //   $(".sticky-wrapper").remove();
-    //   // Moves sticky to be after navcontainer
-    //   $(".navcontainer").after('<div class="sticky-wrapper arc-legacy-stickynav">' + sticky + '</div>');
-    //   // Shows sticky
-    //   $(".arc-legacy-stickynav").show();
+// function stickynav() {
+//   $(document).ready(function () {
+//     document.querySelector(".sticky-wrapper").style.display='none';
+//     // var stickyExists = $(".sticky-wrapper").length > 0;
+//     // console.log("hello from sticky" + stickyExists);
+//     // if (stickyExists) {
+//     //   $(".supernav").css("border-bottom", "none");
+//     //   var sticky = $(".sticky-wrapper").html();
+//     //   $(".sticky-wrapper").remove();
+//     //   // Moves sticky to be after navcontainer
+//     //   $(".navcontainer").after('<div class="sticky-wrapper arc-legacy-stickynav">' + sticky + '</div>');
+//     //   // Shows sticky
+//     //   $(".arc-legacy-stickynav").show();
 
-    //   // Checks to see if its a product
-    //   var isProduct = $(location).attr("href").includes("product");
-    //   console.log(isProduct)
-    //   if (isProduct) {
-    //     var product;
-    //     if ($("h2.block-header").html().includes("ADXMI")) {
-    //       product = "ADXMI";
-    //     } else if ($("h2.block-header").html().includes("ODSS")) {
-    //       product = "ODSS";
-    //     } else if (
-    //       $("h2.block-header")
-    //         .html()
-    //         .includes("ARC's IRS 1099 Processing Service")
-    //     ) {
-    //       product = "IRS 1099 Processing Service";
-    //     } else {
-    //       product = $("h2.block-header").html();
-    //       console.log(product)
-    //     }
-    //     console.log("Product: " + product);
+//     //   // Checks to see if its a product
+//     //   var isProduct = $(location).attr("href").includes("product");
+//     //   console.log(isProduct)
+//     //   if (isProduct) {
+//     //     var product;
+//     //     if ($("h2.block-header").html().includes("ADXMI")) {
+//     //       product = "ADXMI";
+//     //     } else if ($("h2.block-header").html().includes("ODSS")) {
+//     //       product = "ODSS";
+//     //     } else if (
+//     //       $("h2.block-header")
+//     //         .html()
+//     //         .includes("ARC's IRS 1099 Processing Service")
+//     //     ) {
+//     //       product = "IRS 1099 Processing Service";
+//     //     } else {
+//     //       product = $("h2.block-header").html();
+//     //       console.log(product)
+//     //     }
+//     //     console.log("Product: " + product);
 
-    //     $(".sticky-nav__linksList").before(
-    //       "<div class='sticky-nav__Branding'><img src='https://www2.arccorp.com/globalassets/arc-logos/corporate-logos/arc-logo-s-white.png' alt='ARC Logo' class='sticky-nav__arcLogo'><div class='sticky-nav__Productname'>" +
-    //         product +
-    //         "</div></div>"
-    //     );
-    //   }
+//     //     $(".sticky-nav__linksList").before(
+//     //       "<div class='sticky-nav__Branding'><img src='https://www2.arccorp.com/globalassets/arc-logos/corporate-logos/arc-logo-s-white.png' alt='ARC Logo' class='sticky-nav__arcLogo'><div class='sticky-nav__Productname'>" +
+//     //         product +
+//     //         "</div></div>"
+//     //     );
+//     //   }
 
-    //   else {
-    //     var title = $(".mainTitle").html();
-    //     console.log(title);
+//     //   else {
+//     //     var title = $(".mainTitle").html();
+//     //     console.log(title);
 
-    //     $(".sticky-nav__linksList").before(
-    //       "<div class='sticky-nav__Branding'><div class='sticky-nav__PageTitle'>" +
-    //         title +
-    //         "</div></div>"
-    //     );
-    //   }
-    // }
-  });
-}
+//     //     $(".sticky-nav__linksList").before(
+//     //       "<div class='sticky-nav__Branding'><div class='sticky-nav__PageTitle'>" +
+//     //         title +
+//     //         "</div></div>"
+//     //     );
+//     //   }
+//     // }
+//   });
+// }
 
-stickynav();
+ARCStickyNav();
