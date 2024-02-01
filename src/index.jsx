99b016c0c5ee85b2327e8748eaf9fd7a55bacd3c
Ian Fajardo <ifajardo@arccorp.com>
2024-02-01 16:34:23 -0500

    update for ie 11 package

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import Apphome from "./apphome.jsx";
-import MainNavigation from "./components/MainNavigation";
 
 var mountNode = document.getElementById("home-app");
 
