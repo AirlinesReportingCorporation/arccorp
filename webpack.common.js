3194618c163349e2648ac2968721c37212854d40
Ian Fajardo <ifajardo@arccorp.com>
2024-02-01 16:51:28 -0500

    exclube babel rules

--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        //exclude: /node_modules/,
+        exclude: /@babel(?:\/|\\{1,2})runtime|core-js/,
         use: {
           loader: "babel-loader",
           options: {
