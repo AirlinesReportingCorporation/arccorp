d080f700292d073905b0ac82c8ffbea94a348733
Ian Fajardo <ifajardo@arccorp.com>
2024-02-12 14:18:40 -0500

    remove arc online training link in nav

--- src/components/MainNavigation/Training.jsx
+++ src/components/MainNavigation/Training.jsx
@@ -10,35 +10,35 @@ function Training() {
               <a
                 href="/support-training/travel-agencies/?utm_source=Global_Navigation"
                 className="arc-mn-header"
-                >Travel Agencies</a
               >
+                Travel Agencies
+              </a>
               <a
                 href="/products-participation/travel-agencies/agency-participation/arc-specialist-certification/?utm_source=Global_Navigation"
                 className="arc-mn-sub-link"
-                >ARC Specialist Training & Certification</a
               >
+                ARC Specialist Training & Certification
+              </a>
               <a
                 href="https://www2.arccorp.com/support-training/travel-agencies/iata-travel-tourism-diploma-courses/?utm_source=Global_Navigation"
                 className="arc-mn-sub-link"
-                >ARC/IATA Training</a
               >
+                ARC/IATA Training
+              </a>
 
               <a
                 href="/support-training/travel-agencies/custom-training/?utm_source=Global_Navigation"
                 className="arc-mn-sub-link"
-                >Agent Custom Training</a
               >
+                Agent Custom Training
+              </a>
 
               <a
                 href="/support-training/travel-agencies/Self-help-training/?utm_source=Global_Navigation"
                 className="arc-mn-sub-link"
-                >Self-Paced Training Knowledge Center</a
               >
-              <a
-                href="/support-training/arc-online-training/?utm_source=Global_Navigation"
-                className="arc-mn-sub-link"
-                >ARC Online Training</a
-              >
+                Self-Paced Training Knowledge Center
+              </a>
             </div>
             {/* <div className="arc-mn-sub-main col-md-6">
                <a
@@ -65,7 +65,10 @@ function Training() {
           </div>
         </div>
       </div>
-      <div className="col-md-6 arc-mn-sub-feature" style={{"background": "#f6f6f6"}}>
+      <div
+        className="col-md-6 arc-mn-sub-feature"
+        style={{ background: "#f6f6f6" }}
+      >
         <div className="arc-mn-sub-container arc-mn-sub-container-left">
           <div className="row justify-content-end">
             <div className="arc-mn-sub-main col-md-6">
@@ -73,8 +76,9 @@ function Training() {
                 <a
                   href="https://www2.arccorp.com/articles-trends/on-demand-webinars?utm_source=Global_Navigation&utm_campaign=Feature"
                   className="arc-mn-header no-arrow"
-                  >On-Demand Webinars</a
                 >
+                  On-Demand Webinars
+                </a>
                 <div className="arc-mn-webinar-title">
                   Build your knowledge of ARC and the travel industry.
                 </div>
