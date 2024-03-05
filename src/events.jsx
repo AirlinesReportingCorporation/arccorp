1af2b5abdcbf91c0bac538dc5e4533d5991a7c3a
Bree Armstrong <barmstrong@arccorp.com>
2024-03-05 15:18:27 -0500

    add logic for ARC events

--- src/events.jsx
+++ src/events.jsx
@@ -8,9 +8,7 @@ import "core-js/stable";
 
 import LazyLoad from "vanilla-lazyload";
 import { eventsList } from "./js/events";
-import {
-  Stickynav,
-} from "arccorp-vars";
+import { Stickynav } from "arccorp-vars";
 
 if (!document.lazyLoadInstance) {
   document.lazyLoadInstance = new LazyLoad({
@@ -44,40 +42,70 @@ class Events extends Component {
           <div className="event-inner">
             <div className="row no-gutters">
               {eventsList.map((event) => {
-
-                let city = event.location.split(",")[0].replace(" ", "-").toLowerCase()
-                let url = "https://www2.arccorp.com/globalassets/homepage/redesign/events/" + city + ".jpg";
+                let city = event.location
+                  .split(",")[0]
+                  .replace(" ", "-")
+                  .toLowerCase();
+                let url =
+                  "https://www2.arccorp.com/globalassets/homepage/redesign/events/" +
+                  city +
+                  ".jpg";
                 let month = moment(event.date).format("MMM");
-                let day = (moment(event.date).format("D").length != 2 ? "0" + moment(event.date).format("D") : moment(event.date).format("D"));
-                
+                let day =
+                  moment(event.date).format("D").length != 2
+                    ? "0" + moment(event.date).format("D")
+                    : moment(event.date).format("D");
+
                 return (
                   <div className="col-lg-6">
-                    <div className="event-card-outer">
-                      <div className="row">
-                        <div
-                          className="col event-card-left"
-                          style={{
-                            backgroundImage:
-                              "url("+ url + ")",
-                          }}
-                        >
-                          <div className="event-date">
-                            <div className="event-month"><p>{month}</p></div>
-                            <div className="event-day"><p>{day}</p></div>
+                    {event.type != "arc" ? (
+                      <div className="event-card-outer">
+                        <div className="row">
+                          <div
+                            className="col event-card-left"
+                            style={{
+                              backgroundImage: "url(" + url + ")",
+                            }}
+                          >
+                            <div className="event-date">
+                              <div className="event-month">
+                                <p>{month}</p>
+                              </div>
+                              <div className="event-day">
+                                <p>{day}</p>
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div className="col event-card-right">
-                          <div className="event-right-inner">
-                            <p className="event-title">{event.title}</p>
-                            <p className="event-desc">{event.desc}</p>
-                            <p className="event-location"><img className="location-icon" src="https://www2.arccorp.com/globalassets/homepage/redesign/events/location.png"/>{event.location}</p>
-                            <a href={event.url} className="event-cta">
-                              Learn More
-                            </a>
+                          <div className="col event-card-right">
+                            <div className="event-right-inner">
+                              <p className="event-title">{event.title}</p>
+                              <p className="event-desc">{event.desc}</p>
+                              <p className="event-location">
+                                <img
+                                  className="location-icon"
+                                  src="https://www2.arccorp.com/globalassets/homepage/redesign/events/location.png"
+                                />
+                                {event.location}
+                              </p>
+                              <a href={event.url} className="event-cta">
+                                Learn More
+                              </a>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    ) : (
+                      <div className="event-card-outer" style={{height: "100%"}}>
+                        <a href={event.url}>
+                          <div
+                            className="arc-event"
+                            style={{
+                              backgroundImage: "url(" + event.imgUrl + ")",
+                            }}
+                          ></div>
+                        </a>
+                      </div>
+                    )}
                   </div>
                 );
               })}
