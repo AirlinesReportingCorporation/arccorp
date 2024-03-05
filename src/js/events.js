f32d69c13a3ac98c37d0f73af91f2c2b80ea8cc9
Bree Armstrong <barmstrong@arccorp.com>
2024-03-05 15:06:07 -0500

    update styles for event cards and fill in the year's events

--- src/js/events.js
+++ src/js/events.js
@@ -4,97 +4,112 @@ export const eventsList= [
         title: "UATP's Airline Distribution 2024",
         desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
         location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        url: "https://uatp.com/airline-distribution/",
+        type: "industry"
     },
     {
         date: "Apr 2, 2024",
         title: "Connect Spring Marketplace 2024",
         desc: "Unlock unparalleled opportunities for growth and success at Connect Spring Marketplace, the paramount event designed exclusively for event professionals.",
         location:"Las Vegas, NV",
-        url: "https://www.connectmeetings.com/events/connect-spring-marketplace-2024"
+        url: "https://www.connectmeetings.com/events/connect-spring-marketplace-2024",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Unlock unparalleled opportunities for growth and success at Connect Spring Marketplace, the paramount event designed exclusively for event professionals.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Apr 9, 2024",
+        title: "Elevate + TravelConnect 2024",
+        desc: "ARC and ATPCO are coming together to serve the travel industry with Elevate + TravelConnect, a jointly hosted conference in Washington, DC, on April 9-11, 2024. ",
+        location:"Washington, DC",
+        url: "https://elevate-travelconnect.com/",
+        imgUrl: "https://www2.arccorp.com/globalassets/homepage/ETC-RegOpen-jumbotron.jpg",
+        type: "arc"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "May 3, 2024",
+        title: "IPW",
+        desc: "U.S. Travel Association’s IPW will continue to reunite and rebuild the global travel industry, providing an in-person platform to conduct business and renew our relationships around the world.",
+        location:"Los Angeles, CA",
+        url: "https://www.ipw.com/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "May 20, 2024",
+        title: "ACI-NA Jumpstart Air Service Development Conference",
+        desc: "ACI-NA’s JumpStart® Air Service Development Conference is North America’s premier air service development conference. ",
+        location:"Washington, DC",
+        url: "https://airportscouncil.org/conference/2024-jumpstart-air-service-development-conference/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "May 29, 2024",
+        title: "The ASTA Travel Advisor Conference 2024",
+        desc: "Known as the “Essential Industry Event for Travel Agency Professionals”, the annual ASTA Global Convention is where the best of the best in the travel agency industry convene for three days of inspiration, education, and entertainment – guaranteed to leave attendees ready to boost their business.",
+        location:"Dallas, TX",
+        url: "https://traveladvisorconference.org/index.cfm",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Jun 2, 2024",
+        title: "IATA's Annual General Meeting (AGM) and World Air Transport Summit",
+        desc: "The Annual General Meeting (AGM) is IATA's pre-eminent event. In addition to its governance role as IATA's ultimate authority, the AGM provides a focus for emerging industry issues and a forum for members to meet and network.",
+        location:"Dubai, United Arab Emirates",
+        url: "https://www.iata.org/en/events/agm/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Jul 16, 2024",
+        title: "Destinations International Annual Convention",
+        desc: "Annual Convention is the destination organization industry's premier event to meet your peers and gain cutting-edge knowledge to tackle the challenges of tomorrow.",
+        location:"Tampa, FL",
+        url: "https://destinationsinternational.org/event/annual-convention",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Jul 22, 2024",
+        title: "GBTA Convention 2024",
+        desc: "GBTA Convention 2024 stands as The Business Event of the Year® for professionals in business travel. It serves as a paramount gathering where innovation and learning about products and services take center stage. Come join us in Atlanta, Georgia, from July 22-24, 2024!",
+        location:"Atlanta, GA",
+        url: "https://convention.gbta.org/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Sep 4, 2024",
+        title: "DMA West Education Summit",
+        desc: "DMA West is here to support and provide resources to western DMOs as they enhance initiatives and technologies associated with interactions with visitors, clients, partners/stakeholders, board members and elected leaders, as well as interactions with colleagues and industry peers.",
+        location:"Eugene, OR",
+        url: "https://www.dmawest.org/education/education-summit/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Sep 17, 2024",
+        title: "Skift Global Forum 2024",
+        desc: "The premier thought leadership conference covering the business of global travel - Skift Global Forum - is heading back to New York City for its eleventh year in September 2024. This highly anticipated event promises to be bigger and better than ever before, bringing together industry experts, influencers, and trailblazers from around the world.",
+        location:"New York, NY",
+        url: "https://live.skift.com/skift-global-forum-2024/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Oct 16, 2024",
+        title: "​ASTA Premium Business Summit",
+        desc: "Ahoy! Join ASTA and host sponsor, Virgin Voyages, on the Valiant Lady for our annual Premium Business Summit, October 16-20, 2024.",
+        location:"Miami, FL",
+        url: "https://s1.goeshow.com/asta/PBS24/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Oct 23, 2024",
+        title: "APG World Connect Conference",
+        desc: "Every year, APG holds the World Connect airline distribution conference, attended by almost 500 senior representatives from the airline industry including around 100 senior airline managers, representatives of global travel groups as well as many other senior representatives from different sectors of the aviation industry.",
+        location:"Malta",
+        url: "https://worldconnect.apg-ga.com/",
+        type: "industry"
     },
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/"
+        date: "Oct 28, 2024",
+        title: "IATA World Passenger Symposium (WPS) and World Financial Symposium (WFS)",
+        desc: "The joint IATA World Financial Symposium and IATA World Passenger Symposium event is where airline retailing, finance and passenger experience unite to create a dynamic platform for strategic exploration, actionable insights, and transformative innovation.",
+        location:"Bangkok, Thailand",
+        url: "https://www.iata.org/en/events/all/wfs--wps/",
+        type: "industry"
     },
 ]
