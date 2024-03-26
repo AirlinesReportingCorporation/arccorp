2500d757e3b1c2da5d4d89c58060037fdf0b03d8
Bree Armstrong <barmstrong@arccorp.com>
2024-03-26 15:11:50 -0400

    remove old event

--- src/pages/arc-events/events.js
+++ src/pages/arc-events/events.js
@@ -1,13 +1,5 @@
 export const eventsList= [
     {
-        date: "Mar 12, 2024",
-        title: "UATP's Airline Distribution 2024",
-        desc: "Airline Distribution® 2024 is UATP’s annual interactive conference that focuses on important distribution issues affecting the airline industry.",
-        location:"Vienna, Austria",
-        url: "https://uatp.com/airline-distribution/",
-        type: "industry"
-    },
-    {
         date: "Apr 2, 2024",
         title: "Connect Spring Marketplace 2024",
         desc: "Unlock unparalleled opportunities for growth and success at Connect Spring Marketplace, the paramount event designed exclusively for event professionals.",
